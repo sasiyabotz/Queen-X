1096c97a1b4d1fd38aaebffa4f181bacbffb00ca
sasiyabotz <100249013+sasiyabotz@users.noreply.github.com>
2023-02-14 18:55:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
-global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94768953416'] //ur owner number
+global.ownername = "©𝘀𝗮𝘀𝗶𝗿𝘂" //ur owner name
+global.ytname = "අයියෝ සාමි" //ur yt chanel name
+global.socialm = "🏻©sasiru" //ur github or insta name
+global.location = "sri lanka, north central province, anuradhapura" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
+global.ownernomer = "94768953416" //ur number
+global.premium = ['94768953416'] //ur premium number
+global.botname = '©sasiru_bota' //ur bot name
+global.linkz = "https://chat.whatsapp.com/L6jWRWQJemx4ZqllhaatND" //your theme url which will be displayed on whatsapp
 global.websitex = "https:// no" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
-global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.themeemoji = "🔮" //ur theme emoji
+global.packname = "🏻©sasiru 💥" //ur sticker watermark packname
+global.author = "   🏻©sasiru_bota whatsapp bot developed by\©sasiru" //ur sticker watermark author
+global.wm = "🏻💥sasiru." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -48,7 +48,7 @@ global.mess = {
     group: '*👸🏻💭Features Used Only For Groups!*',
     private: '*👸🏻💭Features Used Only For Private Chat!*',
     bot: '*👸🏻💭This feature could be used by bot only*',
-    wait: '*👸🏻💭Please wait...*',
+    wait: '*💥පොඩ්ඩක් හිටපන් බන්🔮',
     linkm: '*👸🏻💭Where is the link?*',
     endLimit: '*👸🏻💭Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours*',
     nsfw: '*👸🏻💭The nsfw feature has not been activated, please contact the admin to activate*',
